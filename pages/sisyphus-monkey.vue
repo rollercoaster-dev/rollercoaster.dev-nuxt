ecf3c2e3808395032b952fe59dfec7343c67b8fd
Joe Czarnecki <28707072+joeczar@users.noreply.github.com>
2024-06-19 21:32:12 +0200

    chore: added dictionary log in modal

--- pages/sisyphus-monkey.vue
+++ pages/sisyphus-monkey.vue
@@ -18,22 +18,20 @@ import sisyphusMonkey from "@/assets/images/sisyphus-monkey.png"
       url:sisyphusMonkey,
       alt:'A vast, surreal landscape filled with countless typewriters, each operated by a monkey. Among them, one monkey has written a masterpiece resembling Shakespeare\'s work. In the center, a modern, intricate machine tirelessly generates poems, reminiscent of Stanislaw Lem\'s imaginative stories. Overseeing this scene is a figure clearly depicting Sisyphus, endlessly pushing a giant boulder up a hill, symbolizing the futility and repetition. The background is an abstract blend of literary elements and mechanical parts, creating a fusion of classical and futuristic aesthetics., photo, analog style, photorealistic, 100mm lens'
       }"
-      :CTA="{
-      label: 'Add your definition!',
-      url:'/sisyphus-monkey/dictionary'
-      }"
+     
     />
+    <router-view v-slot="{ Component }">
+      <transition name="modal">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+
     <div class="w-full max-w-screen-md px-4 mx-auto my-4">
       <p>text about the exhibition</p>
       <p>text about the piece</p>
-      <p>text about rollercoaster.dev's take on the piece</p>/
+      <p>text about rollercoaster.dev's take on the piece</p>
     </div>
   </div>
-  <router-view v-slot="{ Component }">
-    <transition name="modal">
-      <component :is="Component" />
-    </transition>
-  </router-view>
 </template>
 
 <style scoped>
--- pages/sisyphus-monkey/dictionary.vue
+++ pages/sisyphus-monkey/dictionary.vue
@@ -1,38 +1,51 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-const closeModal = () => {
-  router.push({ path: '/sisyphus-monkey' });
-};
+// import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 </script>
 
 <template>
-  <div class="modal-overlay" @click="closeModal">
-    <div class="modal-content" @click.stop>
-      <h2>Modal Content</h2>
-      <p>This is a modal.</p>
-      <button @click="closeModal">Close</button>
-    </div>
-  </div>
+  <Dialog>
+    <DialogTrigger as-child>
+      <Button class="mx-auto">
+        Add your definition!
+      </Button>
+    </DialogTrigger>
+    <DialogContent class="sm:max-w-[425px]">
+      <DialogHeader>
+        <DialogTitle>Edit profile</DialogTitle>
+        <DialogDescription>
+          Make changes to your profile here. Click save when you're done.
+        </DialogDescription>
+      </DialogHeader>
+      <div class="grid gap-4 py-4">
+        <div class="grid grid-cols-4 items-center gap-4">
+          <Label for="name" class="text-right">
+            Name
+          </Label>
+          <Input id="name" value="Pedro Duarte" class="col-span-3" />
+        </div>
+        <div class="grid grid-cols-4 items-center gap-4">
+          <Label for="username" class="text-right">
+            Username
+          </Label>
+          <Input id="username" value="@peduarte" class="col-span-3" />
+        </div>
+      </div>
+      <DialogFooter>
+        <Button type="submit">
+          Save changes
+        </Button>
+      </DialogFooter>
+    </DialogContent>
+  </Dialog>
 </template>
-
-<style scoped>
-.modal-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.modal-content {
-  background: white;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-}
-</style>
